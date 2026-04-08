cff2664e866d7fd592169a43134816d5dd3058de
enzocaravano-gk <enzocaravano@gmail.com>
2026-04-08 13:49:40 +0200

    Update sonner.tsx
    Fix export Toaster

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,17 +1,2 @@
-// src/components/ui/sonner.tsx
-// Componente placeholder per le notifiche Sonner
-// Questo evita errori di build se il componente originale manca
-
-import { Toaster } from 'sonner';
-
-interface SonnerProps {
-  // Aggiungi qui le props se necessarie, altrimenti lascia vuoto
-}
-
-export function Sonner({}: SonnerProps) {
-  return (
-    <Toaster richColors position="top-right" />
-  );
-}
-
-export default Sonner;
+// Esporta direttamente il componente Toaster dal pacchetto sonner
+export { Toaster } from "sonner";
